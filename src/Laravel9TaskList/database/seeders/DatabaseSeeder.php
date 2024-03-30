5abf4cdb7035ef6adffe773d57e347da0a1a7d5c
ISAWASHUN <syun20000616@icloud.com>
2024-03-30 16:23:50 +0900

    Taskの一覧機能を作成

--- src/Laravel9TaskList/database/seeders/DatabaseSeeder.php
+++ src/Laravel9TaskList/database/seeders/DatabaseSeeder.php
@@ -20,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(FoldersTableSeeder::class);
+        $this->call(TasksTableSeeder::class);
     }
 }
