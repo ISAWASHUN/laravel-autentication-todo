45353097e468a2b6f8e8398c0ce625253383f36c
ISAWASHUN <syun20000616@icloud.com>
2024-04-01 00:06:34 +0900

    Taskの作成機能

--- src/Laravel9TaskList/app/Http/Controllers/TaskController.php
+++ src/Laravel9TaskList/app/Http/Controllers/TaskController.php
@@ -28,4 +28,25 @@ class TaskController extends Controller
             'tasks' => $tasks,
         ]);
     }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id,
+        ]);
+    }
+
+    public function create(int $id, Request $request)
+    {
+        $folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+        $folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
+    }
 }
